6e909570a3f73d2727fbdac01876204225221e49
Manabu Kiyota <agmk4655@gmail.com>
2022-05-09 07:09:17 +0000

    add data augment func

--- clone.py
+++ clone.py
@@ -6,10 +6,90 @@ import datetime
 import h5py
 import numpy as np
 
+import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Dropout, MaxPooling2D
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+
+
+# ----------------------------------------------------------------
+# add for data augument
+import pandas as pd
+import random
+import ntpath
+from sklearn.utils import shuffle
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+
+
+def zoom(image):
+    zoom = iaa.Affine(scale=(1, 1.3))
+    image = zoom.augment_image(image)
+    return image
+
+def pan(image):
+    pan = iaa.Affine(translate_percent= {"x" : (-0.1, 0.1), "y": (-0.1, 0.1)})
+    image = pan.augment_image(image)
+    return image
+
+def img_random_brightness(image):
+    brightness = iaa.Multiply((0.2, 1.2))
+    image = brightness.augment_image(image)
+    return image
+
+def img_random_flip(image, steering_angle):
+    image = cv2.flip(image,1)
+    steering_angle = -steering_angle
+    return image, steering_angle
+
+def random_augment(image, steering_angle):
+    image = mpimg.imread(image)
+    if np.random.rand() < 0.5:
+        image = pan(image)
+    if np.random.rand() < 0.5:
+        image = zoom(image)
+    if np.random.rand() < 0.5:
+        image = img_random_brightness(image)
+    if np.random.rand() < 0.5:
+        image, steering_angle = img_random_flip(image, steering_angle)
+
+    return image, steering_angle
+
+
+def img_preprocess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+def batch_generator(image_paths, steering_ang, batch_size, istraining):
+
+    while True:
+        batch_img = []
+        batch_steering = []
+
+        for i in range(batch_size):
+            
+            random_index = random.randint(0, len(image_paths) - 1)
+           
+            if istraining:
+                im, steering = random_augment(image_paths[random_index], steering_ang[random_index])
+            else:
+                im = mpimg.imread(image_paths[random_index])
+                steering = steering_ang[random_index]
+            
+            im = img_preprocess(im)
+            batch_img.append(im)
+            batch_steering.append(steering)
+        yield (np.asarray(batch_img), np.asarray(batch_steering))
+
+
+# ----------------------------------------------------------------
+
 
 def getrowsFromDrivingLogs(dataPath):
     rows = []
@@ -42,16 +122,39 @@ def getImagesAndSteerings(rows):
     
     return (np.array(images), np.array(steerings))
 
+
+# def trainModelAndSave(model, inputs, outputs, epochs, batch_size):
+    
+#     X_train, X_valid, y_train, y_valid = train_test_split(inputs, outputs, test_size=0.2, shuffle=True)
+#     #Setting model
+#     model.compile(loss='mean_squared_error', optimizer=Adam(lr=1.0e-3))
+#     #Learning model
+#     model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=epochs, verbose=1, validation_data=(X_valid, y_valid))
+#     #Saving model
+#     model.save('model.h5')
+
+    
 def trainModelAndSave(model, inputs, outputs, epochs, batch_size):
     
     X_train, X_valid, y_train, y_valid = train_test_split(inputs, outputs, test_size=0.2, shuffle=True)
     #Setting model
     model.compile(loss='mean_squared_error', optimizer=Adam(lr=1.0e-3))
     #Learning model
-    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=epochs, verbose=1, validation_data=(X_valid, y_valid))
+    #model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=epochs, verbose=1, validation_data=(X_valid, y_valid))
+    
+    history = model.fit_generator(batch_generator(X_train, y_train, 100, 1),
+                              steps_per_epoch=300,
+                              epochs=10,
+                              validation_data=batch_generator(X_valid, y_valid, 100, 0),
+                              validation_steps=200,
+                              verbose=1,
+                              shuffle = 1)
+    
     #Saving model
     model.save('model.h5')
-    
+
+
+
 #essential network
 def nn_model():
     model = Sequential()
@@ -87,15 +190,18 @@ def nvidia_model():
     model.add(Flatten())
     
     model.add(Dense(100, activation='elu'))
-    model.add(Dropout(0.5))
+#    model.add(Dropout(0.5))
 
     model.add(Dense(50, activation='elu'))
-    model.add(Dropout(0.5))
+#    model.add(Dropout(0.5))
 
     model.add(Dense(10, activation='elu'))
-    model.add(Dropout(0.5))
+#    model.add(Dropout(0.5))
     
     model.add(Dense(1))
+    
+    optimizer = Adam(lr=1e-3)
+    model.compile(loss='mse', optimizer=optimizer)
     return model
 
 if __name__ == "__main__":
